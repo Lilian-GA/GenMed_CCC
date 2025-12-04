ab8370b49a2c3012957c6bda5ca53d17d876f4d1
hmdang <hieu-minh.dang@insa-lyon.fr>
2025-12-04 01:18:25 +0100

    corriger spatial data preprocessing

---
+++ spatial_data.R
@@ -0,0 +1,35 @@
+library(Seurat)
+library(SeuratDisk)
+library(reticulate)
+
+# Charger l'objet
+obj <- readRDS("data/data_rds/HT224P1-S1Fc2U1Z1Bs1-SeuratObj.rds")
+DefaultAssay(obj) <- "Spatial"
+
+# Créer un nouvel objet Seurat minimal pour Scanpy
+new_obj <- CreateSeuratObject(
+  counts = obj@assays$Spatial@counts,
+  meta.data = obj@meta.data
+)
+
+# Ajouter un assay Spatial minimal
+new_obj[["Spatial"]] <- CreateAssayObject(counts = obj@assays$Spatial@counts)
+DefaultAssay(new_obj) <- "Spatial"
+
+# Ajouter coordonnées spatial dans un DimReduc (nom libre)
+coords_mat <- as.matrix(obj@images$slice1@coordinates[, c("imagerow", "imagecol")])
+colnames(coords_mat) <- c("spatial_1", "spatial_2")
+
+new_obj[["spatial"]] <- CreateDimReducObject(
+  embeddings = coords_mat,
+  key = "spatial_",
+  assay = "Spatial"
+)
+
+# Ajouter PCA/UMAP si existant
+if (!is.null(obj@reductions$pca)) new_obj[["pca"]] <- obj@reductions$pca
+if (!is.null(obj@reductions$umap)) new_obj[["umap"]] <- obj@reductions$umap
+
+# Exporter h5ad directement via SeuratDisk (sans image)
+SaveH5Seurat(new_obj, filename = "data/HT224P1_minimal.h5seurat", overwrite = TRUE)
+Convert("data/HT224P1_minimal.h5seurat", dest = "h5ad")
